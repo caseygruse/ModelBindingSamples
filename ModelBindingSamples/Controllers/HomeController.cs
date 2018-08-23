4faa9cf5e034d37baef6db531013814c5c48dcf6
caseygruse.222 <caseygruse.222@gmail.com>
2018-08-23 10:51:09 -0700

    added many model binding examples

--- ModelBindingSamples/Controllers/HomeController.cs
+++ ModelBindingSamples/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ModelBindingSamples.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,7 +11,56 @@ namespace ModelBindingSamples.Controllers
 	{
 		public ActionResult Index()
 		{
+			//Passing models to Views
+
+			//ViewBag && ViewData
+			//ViewBag and Data are dynamic and will crash at runtime if types are
+			//incompatible and they use strings.
+
+			//TempData
+			//Just like ViewData EXCEPT data will last for one redirect.
+			//ViewBag data is lost if redirected.
+
+			Instructor instructor = GetInstructor();
+
+			//passing an object through ViewBag && ViewData
+			//ViewBag.Instructor = i;
+			ViewData["Instructor"] = instructor;
+			ViewData["Courses"] = instructor.Courses;
+
+			
 			return View();
+		}
+
+		private Instructor GetInstructor()
+		{
+			Course c1 = new Course()
+			{
+				CourseID = 5,
+				Title = ".Net Web Programming"
+			};
+
+			Course c2 = new Course()
+			{
+				CourseID = 10,
+				Title = "Java 101"
+			};
+			Instructor teach = new Instructor()
+			{
+				InstructorID = 1000,
+				FullName = "Mr. Teacher"
+			};
+			teach.Courses.Add(c1);
+			teach.Courses.Add(c2);
+			return teach;
+		}
+		/// <summary>
+		/// Tuples can return two types at once
+		/// </summary>
+		/// <returns></returns>
+		private Tuple<string, bool> Test()
+		{
+			return new Tuple<string, bool>("97543211", false);
 		}
 
 		public ActionResult About()
@@ -25,6 +75,11 @@ namespace ModelBindingSamples.Controllers
 			ViewBag.Message = "Your contact page.";
 
 			return View();
+		}
+
+		public string GetDate()
+		{
+			return DateTime.Today.ToShortDateString();
 		}
 	}
 }
---
+++ ModelBindingSamples/Models/Instructor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ModelBindingSamples.Models
+{
+	public class Instructor
+	{
+		public Instructor()
+		{
+			Courses = new List<Course>();
+		}
+
+		public int InstructorID { get; set; }
+
+		public string FullName { get; set; }
+
+		public ICollection<Course> Courses { get; set; }
+	}
+}
